7a43e4b4ba0cdca16aecf8a43dcadcf00d6edfd7
Saksham2703 <66508599+Saksham2703@users.noreply.github.com>
2022-11-12 19:44:55 -0600

    Update IBackend.java

--- IBackend.java
+++ IBackend.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public interface IBackend {
+  
+  public void setMajor(String userMajor);
+  
+  public void addUserCourse(String userCourse);
+
+  public ArrayList<Requirements> getMajorRequirements();
+
+  public ArrayList<Course> checkRequirement(Requirements requirements);
 
 }
+
